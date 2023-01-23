df50f75ef03977ad8a0cbd4beb4a0baa051f92de
Mudassir <mudassir45khan@yahoo.com>
2023-01-23 18:30:20 +0800

    feat: setup for buildSrc

---
+++ buildSrc/src/main/kotlin/mudassir/dependencies/Dependencies.kt
@@ -0,0 +1,81 @@
+package mudassir.dependencies
+
+object Dependencies {
+    // Core
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraint_layout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
+    const val activity_ktx = "androidx.activity:activity-ktx:${Versions.activity_ktx}"
+    const val core_ktx = "androidx.core:core-ktx:${Versions.core_ktx}"
+    const val android_annotation = "androidx.annotation:annotation:${Versions.android_annotation}"
+
+
+    //Navigation
+    const val navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+
+    // Networking
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val okHttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+    const val logging = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+    const val gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    const val moshi_converter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+
+    // Coroutines
+    const val coroutines_core =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+    const val coroutines_android =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+
+    //Moshi
+    const val moshi ="com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
+    const val moshi_adapter= "com.serjltt.moshi:moshi-lazy-adapters:${Versions.moshi_adapter}"
+
+    // Dagger
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val dagger_android = "com.google.dagger:dagger-android:${Versions.dagger}"
+    const val dagger_android_support = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+    const val dagger_android_processor =
+        "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+    const val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    const val javax_annotation = "org.glassfish:javax.annotation:${Versions.javax}"
+
+    // Lifecycle
+    const val lifecycle_runtime = "androidx.lifecycle:lifecycle-runtime:${Versions.lifecycle}"
+    const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
+    const val lifecycle_viewmodel_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    const val lifecycle_livedata_ktx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+    const val lifecycle_java8 = "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle}"
+
+    // Timber
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+
+    // Glide
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val glide_compiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+    const val glide_okhttp = "com.github.bumptech.glide:okhttp3-integration:${Versions.glide}"
+}
+
+object TestDependencies {
+    // Core library
+    const val test_core = "androidx.test:core:${Versions.test_core}"
+    const val arch_core = "androidx.arch.core:core-testing:${Versions.arch_core}"
+    const val coroutines_core =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines_core}"
+
+    // AndroidJUnitRunner and JUnit Rules
+    const val runner = "androidx.test:runner:${Versions.runner}"
+    const val rules = "androidx.test:rules:${Versions.rules}"
+
+    // Assertions
+    const val junit = "androidx.test.ext:junit:${Versions.junit}"
+    const val truth_ext = "androidx.test.ext:truth:${Versions.truth_ext}"
+
+    // Espresso dependencies
+    const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso_core}"
+
+    // Third party
+    const val mockK = "io.mockk:mockk:${Versions.mockK}"
+}
---
+++ buildSrc/src/main/kotlin/mudassir/dependencies/Versions.kt
@@ -0,0 +1,58 @@
+package mudassir.dependencies
+
+object Versions {
+    // Kotlin
+    const val activity_ktx = "1.6.0"
+    const val core_ktx = "1.7.0"
+    const val android_annotation = "1.2.0"
+    const val kotlin = "1.6.10"
+
+    // UI
+    const val appcompat = "1.6.0"
+    const val material = "1.7.0"
+    const val constraint_layout = "2.1.4"
+
+    //Navigation
+    const val navigation = "2.5.3"
+
+    // Networking
+    const val retrofit = "2.9.0"
+    const val okHttp = "4.9.1"
+
+    // Coroutines
+    const val coroutines = "1.5.2"
+
+    //moshi
+    const val moshi ="1.9.0"
+    const val moshi_adapter ="2.2"
+
+    // Lifecycle
+    const val lifecycle = "2.5.1"
+
+    // DI
+    const val dagger = "2.44.2"
+    const val javax = "10.0-b28"
+
+    // Glide
+    const val glide = "4.12.0"
+
+    // Timber
+    const val timber = "5.0.1"
+
+    // Plugins
+    const val detekt = "1.18.1"
+    const val ktlint = "10.1.0"
+    const val ktlint_internal = "0.42.1"
+    const val jacoco = "0.8.7"
+
+    // Testing
+    const val test_core = "1.4.0"
+    const val runner = "1.4.0"
+    const val rules = "1.4.0"
+    const val junit = "1.1.5"
+    const val truth_ext = "1.4.0"
+    const val espresso_core = "3.5.1"
+    const val mockK = "1.12.0"
+    const val arch_core = "2.1.0"
+    const val coroutines_core = "1.5.2"
+}
